0f840e1a33d170997400416bedb2ed4155640b27
hardikshakya <hardikshakya1997@gmail.com>
2024-01-29 10:53:14 +0530

    feat: added TypeORM configuration and added GRPC transport

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
+import { INestMicroservice, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { config } from 'dotenv';
+
+config();
+
 import { AppModule } from './app.module';
+import { protobufPackage } from './order/proto/order.pb';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app: INestMicroservice = await NestFactory.createMicroservice(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: '0.0.0.0:50052',
+        package: protobufPackage,
+        protoPath: join(
+          'node_modules/quickshop-shared-proto/proto/order.proto',
+        ),
+      },
+    },
+  );
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  await app.listen();
 }
+
 bootstrap();
